1890aa2164898dca9a7fe579f737708b5bb466eb
Totom3 <suptom3@gmail.com>
2018-11-19 16:57:34 -0500

    Base game logic

---
+++ src/space/invaders/GameConstants.java
@@ -0,0 +1,23 @@
+package space.invaders;
+
+/**
+ *
+ * @author Tomer Moran
+ */
+public interface GameConstants {
+
+	int SCREEN_WIDTH = 1200;
+	int SCREEN_HEIGHT = 900;
+
+	int ALIENS_GRID_LENGTH = 11;
+	int ALIENS_GRID_HEIGHT = 5;
+
+	double LEFT_GAME_BOUND = 20;
+	double RIGHT_GAME_BOUND = SCREEN_WIDTH - 20;
+
+	double ALIEN_SIZE = 60;
+	double ALIEN_SPACING_H = 15;
+	double ALIEN_SPACING_V = 25;
+	
+	Vec2D START_ALIEN_POSITION = new Vec2D(LEFT_GAME_BOUND, 200);
+}
