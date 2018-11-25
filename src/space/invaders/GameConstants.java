a2c950e1b5aff91490cd3205dbf7c43191abae20
Totom3 <suptom3@gmail.com>
2018-11-25 18:36:28 -0500

    Group constant pairs as vectors and introduce TPS & FPS constants

--- src/space/invaders/GameConstants.java
+++ src/space/invaders/GameConstants.java
@@ -6,26 +6,31 @@ package space.invaders;
  */
 public interface GameConstants {
 
-	int SCREEN_WIDTH = 1200;
-	int SCREEN_HEIGHT = 900;
+	Vec2D SCREEN_SIZE = new Vec2D(1200, 900);
 
 	int ALIENS_GRID_LENGTH = 11;
 	int ALIENS_GRID_HEIGHT = 5;
 
 	double LEFT_GAME_BOUND = 20;
-	double RIGHT_GAME_BOUND = SCREEN_WIDTH - 20;
+	double RIGHT_GAME_BOUND = SCREEN_SIZE.getX() - 20;
 
-	double ALIEN_SIZE = 50;
-	double ALIEN_SPACING_H = 35;
-	double ALIEN_SPACING_V = 30;
-	
-	double SPACESHIP_Y = SCREEN_HEIGHT - 50;
-	double SPACESHIP_WIDTH = 60;
-	double SPACESHIP_HEIGHT = 40;
-	
-	
+	Vec2D ALIEN_SIZE = new Vec2D(50, 50);
+	Vec2D ALIEN_SPACING = new Vec2D(30, 35);
+	Vec2D SPACESHIP_SIZE = new Vec2D(60, 40);
+	double SPACESHIP_Y = SCREEN_SIZE.getY() - 50;
+
+	// Game ticks per second
+	double GAME_TPS = 100;
+
+	// Frames per second
+	double RENDER_FPS = 60;
+
+	// Start position of aliens
 	Vec2D START_ALIEN_POSITION = new Vec2D(LEFT_GAME_BOUND, 120);
-	Vec2D ALIEN_MOVE_LEFT = new Vec2D(20, 0);
-	Vec2D ALIEN_MOVE_RIGHT = new Vec2D(-20, 0);
-	Vec2D ALIEN_MOVE_DOWN = new Vec2D(0, 20);
+
+	// Horizontal speed of aliens, in pixels per second.
+	Vec2D ALIEN_MOVEMENT_SPEED = new Vec2D(20, 0).divide(GAME_TPS);
+
+	// The vertical jump aliens do when reaching the end of the screen
+	Vec2D ALIEN_DOWN_JUMP = new Vec2D(0, 20);
 }
