d47df2e2e4694cd597da1574c458c232b40e9dbf
Totom3 <suptom3@gmail.com>
2018-11-25 15:32:57 -0500

    Little code cleanup

--- src/space/invaders/AssetManager.java
+++ src/space/invaders/AssetManager.java
@@ -4,28 +4,32 @@ package space.invaders;
  *
  * @author Frankie
  */
-import java.io.File;
 import java.util.EnumMap;
 import java.util.Map;
 import javafx.scene.image.Image;
 import javafx.scene.media.AudioClip;
-import static jdk.nashorn.internal.objects.NativeJava.type;
 
 public class AssetManager {
 
-	private static final Map<ImageResources, Image> imageMap = new EnumMap<>(ImageResources.class);
+	private static boolean loadedResources;
 
+	private static final Map<ImageResources, Image> imageMap = new EnumMap<>(ImageResources.class);
 	private static final Map<AudioResources, AudioClip> audioMap = new EnumMap<>(AudioResources.class);
 
-	public static Image getImage(ImageResources imR) {
-		return imageMap.get(imR);
+	public static Image getImage(ImageResources image) {
+		return imageMap.get(image);
 	}
 
-	public static AudioClip getSound(AudioResources auR) {
-		return audioMap.get(auR);
+	public static AudioClip getSound(AudioResources audio) {
+		return audioMap.get(audio);
 	}
 
-	public void initialize() {
-		
+	public static void loadResources() {
+		if (loadedResources)
+			throw new IllegalStateException("resources already loaded");
+
+		// TODO: load resources
+		loadedResources = true;
 	}
+
 }
